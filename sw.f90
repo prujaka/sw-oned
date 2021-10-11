02efcf5a295ad2b333fad6bbcc4ed715fe872818
Sergey Tkachenko <sergeyprujina@gmail.com>
2021-10-11 17:57:52 +0200

    added boundary conditions with velocity switch

--- sw.f90
+++ sw.f90
@@ -2,15 +2,17 @@ program sw_oned
   implicit none
   integer, parameter :: DP = kind(1.0d0)
   integer, parameter :: N = 100
-  real(DP), parameter :: gg = 9.81d0
   real(DP), parameter :: L = 100.0d0
   real(DP), parameter :: XL = 0.0d0
+  real(DP), parameter :: GG = 9.81d0
   real(DP), parameter :: X_DAM = 0.4d0 * L
   real(DP), parameter :: DX = L / DFLOAT(N)
   real(DP), parameter :: DEPTH_LEFT = 1.2d0
   real(DP), parameter :: DEPTH_RIGHT = 1.0d0
   real(DP), parameter :: VELOCITY_LEFT = 1.0d0
   real(DP), parameter :: VELOCITY_RIGHT = 1.0d0
+  real(DP), parameter :: BC_U_LEFT = -1.0d0
+  real(DP), parameter :: BC_U_RIGHT = -1.0d0
   character(len=20), parameter :: OUTPUT_FILE = 'res.dat'
 
   real(kind=DP), allocatable :: x(:), h(:), u(:)
@@ -19,6 +21,7 @@ program sw_oned
   call set_mesh(x)
   call set_ic_dambreak(x, h, u)
   call output_solution(OUTPUT_FILE, x, h, u)
+  call set_bc(h, u)
 
   deallocate(x, h, u)
 
@@ -47,6 +50,15 @@ subroutine set_ic_dambreak(x, h, u)
       u(i)=VELOCITY_RIGHT
     endif
   enddo
+end
+
+subroutine set_bc(h, u)
+  real(DP), intent(inout) :: h(0: N+1), u(0: N+1)
+
+  h(0) = h(1)
+  u(0) = BC_U_LEFT * u(1)
+  h(N+1) = h(N)
+  u(N+1) = BC_U_RIGHT * u (N)
 end
 
 subroutine output_solution(filename, x, h, u)
