6daef492ab48dd827769ed68ed3a6441cc11f8f4
Sergey Tkachenko <sergeyprujina@gmail.com>
2021-10-11 21:38:49 +0200

    added full while loop

--- sw.f90
+++ sw.f90
@@ -1,9 +1,9 @@
 program sw_oned
   implicit none
   integer, parameter :: DP = kind(1.0d0)
-  integer, parameter :: ITER_MAX = 100000000
-  integer, parameter :: N = 100
-  real(DP), parameter :: TIME_FIN = 1.0d0
+  integer, parameter :: ITER_MAX = 100
+  integer, parameter :: N = 1000
+  real(DP), parameter :: TIME_FIN = 20.0d0
   real(DP), parameter :: CFL = 0.9d0
   real(DP), parameter :: L = 100.0d0
   real(DP), parameter :: XL = 0.0d0
@@ -14,34 +14,40 @@ program sw_oned
   real(DP), parameter :: DEPTH_RIGHT = 1.0d0
   real(DP), parameter :: VELOCITY_LEFT = 1.0d0
   real(DP), parameter :: VELOCITY_RIGHT = 1.0d0
-  real(DP), parameter :: BC_U_LEFT = -1.0d0
-  real(DP), parameter :: BC_U_RIGHT = -1.0d0
+  real(DP), parameter :: BC_U_LEFT = 1.0d0
+  real(DP), parameter :: BC_U_RIGHT = 1.0d0
   character(len=20), parameter :: OUTPUT_FILE = 'res.dat'
 
-  integer :: it
+  integer :: it=0
   real(DP) :: dt=1.d-8, time=0.0d0
   real(DP) :: t_start, t_end
-  real(kind=DP), allocatable :: x(:), h(:), u(:)
+  real(DP), allocatable :: x(:), h(:), u(:), h_cons(:), u_cons(:)
+  real(DP), allocatable :: h_flux(:), u_flux(:)
 
-  allocate(x(0:N+1), h(0:N+1), u(0:N+1))
+  allocate(x(0:N+1), h(0:N+1), u(0:N+1), h_cons(0:N+1), u_cons(0:N+1))
+  allocate(h_flux(0:N+1), u_flux(0:N+1))
 
   call cpu_time(t_start)
 
   call set_mesh(x)
   call set_ic_dambreak(x, h, u)
-  call output_solution(OUTPUT_FILE, x, h, u)
-  call set_bc(h, u)
   do while(time<TIME_FIN)
     if (it.ge.ITER_MAX) exit
-    dt = timestep(h,u)
+    dt = timestep(h, u)
     if (dt>TIME_FIN-time) dt = TIME_FIN-time
+    call set_bc(h, u)
+    call riemann_fluxes(h, u, h_flux, u_flux)
+    call prim_to_cons(h, u, h_cons, u_cons)
+    call godunov(h_cons, u_cons, h_flux, u_flux)
+    call cons_to_prim(h_cons, u_cons, h, u)
 
     it=it+1
     time=time+dt
   enddo
+  call output_solution(OUTPUT_FILE, x, h, u)
 
   call cpu_time(t_end)
-  deallocate(x, h, u)
+  deallocate(x, h, u, h_cons, u_cons, h_flux, u_flux)
 
   print*, 'cpu time:  ', t_end - t_start
   print*, 'time:      ', time
@@ -82,13 +88,13 @@ subroutine set_bc(h, u)
   h(0) = h(1)
   u(0) = BC_U_LEFT * u(1)
   h(N+1) = h(N)
-  u(N+1) = BC_U_RIGHT * u (N)
+  u(N+1) = BC_U_RIGHT * u(N)
 end
 
 real(DP) function timestep(h,u)
   implicit none
-  real(kind=DP), dimension(0:N+1), intent(in) :: h, u
-  real(kind=DP) :: cmax
+  real(DP), dimension(0:N+1), intent(in) :: h, u
+  real(DP) :: cmax
   integer :: i
 
   cmax = 0.0d0
@@ -96,6 +102,69 @@ real(DP) function timestep(h,u)
     cmax = max( cmax, dabs(u(i)) + dsqrt(GG*h(i)) )
   enddo
   timestep = CFL*dx/cmax
+end
+
+subroutine riemann_fluxes(h, u, h_flux, u_flux)
+  implicit none
+  real(DP), dimension(0:N+1), intent(in) :: h,u
+  real(DP), dimension(0:N+1), intent(out) :: h_flux, u_flux
+  real(DP) :: hl,ul,hr,ur,f1,f2
+  integer :: i
+
+  do i=0,N
+    hl=h(i)
+    ul=u(i)
+
+    hr=h(i+1)
+    ur=u(i+1)
+
+    call rusanov(hl, ul, hr, ur, f1, f2)
+    h_flux(i) = f1
+    u_flux(i) = f2
+  enddo
+end
+
+subroutine rusanov(hl,ul,hr,ur,f1,f2)
+  implicit none
+  real(DP), intent(in) :: hl,ul,hr,ur
+  real(DP), intent(out) :: f1,f2
+  real(DP) :: pl,al,pr,ar
+  real(DP) :: h_cons_l, h_cons_r, u_cons_l, u_cons_r
+  real(DP) :: h_flux_l, h_flux_r, u_flux_l, u_flux_r
+  real(DP) :: sl,sr,sl1,sr1,sl2,sr2,smid
+
+  pl = 0.5d0*GG*hl*hl
+  al = DSQRT(GG*hl)
+  pr = 0.5d0*GG*hr*hr
+  ar = DSQRT(GG*hr)
+
+  ! Wave speed estimation
+  ! Davis S.F.(1988), 'Simplified second order Godunov type methods',
+  ! SIAM J. Sci. and Stat. Comput., N3, 445-473.
+  sl1=ul-al; sl2=ur-ar
+  sr1=ul+al; sr2=ur+ar
+  sl=DMIN1(sl1,sl2); sr=DMAX1(sr1,sr2)
+  smid=dmax1(dabs(sl),dabs(sr))
+
+  h_cons_l=hl   ;	  h_flux_l=hl*ul
+  u_cons_l=hl*ul;	  u_flux_l=hl*ul*ul+pl
+
+  h_cons_r=hr   ;   h_flux_r=hr*ur
+  u_cons_r=hr*ur;   u_flux_r=hr*ur*ur+pr
+
+  f1 = 0.5d0*(h_flux_l + h_flux_r) - 0.5d0*smid*(h_cons_r - h_cons_l)
+  f2 = 0.5d0*(u_flux_l + u_flux_r) - 0.5d0*smid*(u_cons_r - u_cons_l)
+end
+
+subroutine godunov(h_cons, u_cons, h_flux, u_flux)
+  implicit none
+  real(DP), dimension(0:N+1), intent(in) :: h_flux, u_flux
+  real(DP), dimension(0:N+1), intent(inout) :: h_cons, u_cons
+  integer :: i
+  do i=1,N
+    h_cons(i) = h_cons(i) - (h_flux(i) - h_flux(i-1)) * dt / dx
+    u_cons(i) = u_cons(i) - (u_flux(i) - u_flux(i-1)) * dt / dx
+  enddo
 end
 
 ! AUX PROCEDURES
